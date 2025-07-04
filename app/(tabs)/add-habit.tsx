046bdf25e459b9c6dca74f8c26bf94289f2cebaf
Claudio <claudio.marino@outlook.com>
2025-07-04 14:21:00 +0200

    Agregar pantallas de "Agregar Hábito" y "Rachas" con diseño básico

---
+++ app/(tabs)/add-habit.tsx
@@ -0,0 +1,35 @@
+import { Link } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function AddHabitScreen() {
+	return (
+		<View style={styles.view}>
+			<Text style={styles.h1}>Hello, This is the login page</Text>
+			<Link
+				href="/"
+				style={styles.button}>
+				Home...
+			</Link>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	view: {
+		flex: 1,
+		gap: 8,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	h1: {
+		textAlign: 'center',
+		margin: 20,
+		fontSize: 40,
+	},
+	button: {
+		textAlign: 'center',
+		backgroundColor: 'coral',
+		color: 'white',
+		padding: 8,
+	},
+})
