cc0c7f78333ed6bb9337cff1a8d6df2f32cb41df
Claudio <claudio.marino@outlook.com>
2025-07-11 14:11:41 +0200

    INITIALIZE STATE FOR HABITS AND COMPLETIONS
    Updated the StreaksScreen component to initialize the state for habits and completed habits as empty arrays. Added a function to calculate streak data based on habit completions.

--- app/(tabs)/streaks.tsx
+++ app/(tabs)/streaks.tsx
@@ -12,8 +12,8 @@ import { Query } from 'react-native-appwrite'
 import { Text } from 'react-native-paper'
 
 export default function StreaksScreen() {
-	const [habits, setHabits] = useState<Habit[]>()
-	const [completedHabits, setCompletedHabits] = useState<HabitCompletions[]>()
+	const [habits, setHabits] = useState<Habit[]>([])
+	const [completedHabits, setCompletedHabits] = useState<HabitCompletions[]>([])
 	const { user } = useAuth()
 
 	useEffect(() => {
@@ -50,6 +50,59 @@ export default function StreaksScreen() {
 			console.log(error)
 		}
 	}
+
+	interface StreakData {
+		streak: number
+		bestStreak: number
+		total: number
+	}
+
+	const getStreaksData = (habitId: string): StreakData => {
+		const habitCompletions = completedHabits
+			?.filter((c) => c.habit_id === habitId)
+			.sort(
+				(a, b) =>
+					new Date(a.completed_at).getTime() -
+					new Date(b.completed_at).getTime()
+			)
+
+		if (habitCompletions?.length === 0) {
+			return { streak: 0, bestStreak: 0, total: 0 }
+		}
+
+		// build streaks data
+		let streak = 0
+		let bestStreak = 0
+		let total = habitCompletions.length
+
+		let lastDate: Date | null = null
+		let currentStreak = 0
+
+		habitCompletions?.forEach((c) => {
+			const date = new Date(c.completed_at)
+			if (lastDate) {
+				const diff =
+					(date.getTime() - lastDate.getTime()) / (1000 * 60 * 60 * 24)
+
+				if (diff <= 1.5) {
+					currentStreak += 1
+				} else {
+					currentStreak = 1
+				}
+			} else {
+				if (currentStreak > bestStreak) bestStreak = currentStreak
+				streak = currentStreak
+				lastDate = date
+			}
+		})
+
+		return { streak, bestStreak, total }
+	}
+
+	const habitStreaks = habits.map((habit) => {
+		const { streak, bestStreak, total } = getStreaksData(habit.$id)
+		return { habit, streak, bestStreak, total }
+	})
 
 	return (
 		<View>
